176a65b15de99f4e6838a8f30f7c63f84761dba2
rupeshchy10 <rupevilary1010@gmail.com>
2025-12-24 00:27:59 +0545

    refactor NavBar component for improved logo structure and click handling

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -50,16 +50,20 @@ const NavBar = ({ theme, setTheme, scrollRef }) => {
 				<div className="mx-6 sm:mx-4 md:mx-10 lg:mx-14 bg-[#005427] dark:bg-gradient-to-r dark:from-[#0f172a] dark:via-[#1e3a8a] dark:to-[#1e40af] text-white flex justify-between items-center  h-[3.25rem] rounded-full shadow-[0_0_25px_rgba(30,64,175,0.4)] backdrop-blur-sm border border-transparent dark:border-[#1e3a8a]/40">
 					{/* Logo */}
 					<a href="#home">
-                        <div className="text-xl sm:text-2xl font-bold m-2 flex justify-center items-center">
-						<span className="bg-yellow-500 dark:bg-gradient-to-r dark:from-[#0c0fb3] dark:via-[#1e3a8a] dark:to-[#25282b] rounded-full w-10 h-10 flex justify-center items-center text-black dark:text-white shadow-[0_0_15px_rgba(59,130,246,0.6)] hover:shadow-[0_0_25px_rgba(59,130,246,0.9)]  mr-2 border-[#ED1C24] dark:border-2 hover:scale-105 transition-all duration-100">
-							<img src="./Rup_logo.png" alt="logo_pic" className=""/>
-						</span>
-						Rupesh
-						<span className="text-yellow-500 dark:text-[#3b82f6] dark:drop-shadow-[0_0_8px_rgba(59,130,246,0.8)] ml-1">
-							.dev
-						</span>
-					</div>
-                    </a>
+						<div className="text-xl sm:text-2xl font-bold m-2 flex justify-center items-center">
+							<span className="bg-yellow-500 dark:bg-gradient-to-r dark:from-[#0c0fb3] dark:via-[#1e3a8a] dark:to-[#25282b] rounded-full w-10 h-10 flex justify-center items-center text-black dark:text-white shadow-[0_0_15px_rgba(59,130,246,0.6)] hover:shadow-[0_0_25px_rgba(59,130,246,0.9)]  mr-2 border-[#ED1C24] dark:border-2 hover:scale-105 transition-all duration-100">
+								<img
+									src="./Rup_logo.png"
+									alt="logo_pic"
+									className=""
+								/>
+							</span>
+							Rupesh
+							<span className="text-yellow-500 dark:text-[#3b82f6] dark:drop-shadow-[0_0_8px_rgba(59,130,246,0.8)] ml-1">
+								.dev
+							</span>
+						</div>
+					</a>
 
 					{/* Desktop Links */}
 					<ul className="hidden md:flex space-x-8 font-medium text-base">
@@ -111,6 +115,7 @@ const NavBar = ({ theme, setTheme, scrollRef }) => {
 							<li key={menu.label}>
 								<a
 									href={menu.href}
+									onClick={() => setIsOpen(false)}
 									className="hover:text-yellow-500 dark:hover:text-[#60a5fa] cursor-pointer mt-14"
 								>
 									{menu.label}
